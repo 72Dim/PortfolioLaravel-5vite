a04c961c3494e3f1f2c1963eabe56140e2346131
72Dim <kasyanovd972@gmail.com>
2024-11-27 15:52:59 +0200

    Start project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //E:\a_test\laravel-4.loc\app\Http\Controllers\Controller.php
+    // E:\projects\laravel-4.loc\config\app.php
+}
