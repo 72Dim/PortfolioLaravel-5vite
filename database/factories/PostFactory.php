67c1d0a545cc1c86f7f6ea46db50006da64ceabf
72Dim <kasyanovd972@gmail.com>
2025-01-27 20:41:34 +0200

    In PostFactory.php, changed  and fake()

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,12 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
+    public $faker;
     /**
      * Define the model's default state.
      *
@@ -16,13 +18,35 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
-        $w = fake()->word();
+        /* working version
+            $word = fake()->word();
+            return [
+                'title' => $word,                   /* default one word *
+                'content' => fake()->sentence(2),   /* default 160 characters *
+                'description' => $word.'.JPG',
+                'created_at' => fake()->dateTimeBetween(),
+                'updated_at' => fake()->dateTimeBetween(),
+            ];
+        */
+        /* working version
+            $faker = Faker::create();
+            $word = $faker->word();
+            return [
+                'title' => $word,                          /* default one word *
+                'content' => $faker->sentence(2), /* default 160 characters *
+                'description' => $word.'.JPG',
+                'created_at' => $faker->dateTimeBetween(),
+                'updated_at' => $faker->dateTimeBetween(),
+            ];
+        */
+        $this->faker = Faker::create();
+        $word = $this->faker->word();
         return [
-            'title' => $w,                      /* default one word */
-            'content' => fake()->sentence(2),   /* default 160 characters */
+            'title' => $word,                       /* default one word */
+            'content' => $this->faker->sentence(2), /* default 160 characters */
             'description' => $w.'.JPG',
-            'created_at' => fake()->dateTimeBetween(),
-            'updated_at' => fake()->dateTimeBetween(),
+            'created_at' => $this->faker->dateTimeBetween(),
+            'updated_at' => $this->faker->dateTimeBetween(),
         ];
     }
 }
